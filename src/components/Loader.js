79ff522e8c8981dae419d4d3386cf50f3ecd12b1
Tolkinxon <krosstony639@gmail.com>
2023-01-24 21:50:16 +0500

    loader is changed

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,15 +1,7 @@
 export default function Loader() {
-    return (
-<div className="preloader-wrapper active">
-    <div className="spinner-layer spinner-green-only">
-      <div className="circle-clipper left">
-        <div className="circle"></div>
-      </div><div className="gap-patch">
-        <div className="circle"></div>
-      </div><div className="circle-clipper right">
-        <div className="circle"></div>
-      </div>
+  return (
+    <div class="progress">
+      <div class="indeterminate"></div>
     </div>
-</div>
-)
+  )
 }
