7810257a1ad18341de200accf3ce6710545a23ff
Tolkinxon <soliyevtolkinxon@gmail.com>
2023-02-04 16:18:24 +0500

    h2lar

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,61 +1,7 @@
 export default function Loader() {
-
-  
-
   return (
     <div class="progress">
       <div class="indeterminate"></div>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-      <h2>hello wolrd</h2>
-
-      
     </div>
   )
 }
